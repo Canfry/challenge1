ad7c7929a16db8e5ca49fd96741d98bf1d0f655b
fry <fry.land@hotmail.fr>
2023-05-13 06:30:45 +0200

    adding mario challenge

---
+++ mario/README.md
@@ -0,0 +1,5 @@
+# MARIO CHALLENGE
+
+The goal is to reproduce in the terminal the pyramids of bricks that we see on the Mario game (see assets folder).
+
+You have to prompt the user to decide the height of the pyramid (for exemple between 1 and 8), in order to display the pyramid as shown on the assets folder. Let's use hashtags to represent the bricks.
--- mario/mario.c
+++ mario/mario.c
@@ -1,81 +1,49 @@
-// #include <stdio.h>
-// #include <cs50.h>
-
-// int main(void)
-// {
-//   for(int i = 0; i < 4; i++)
-//   {
-//     printf("?");
-//   }
-//   printf("\n");
-// }
-
-
-
-// int main(void)
-// {
-//   for(int i = 0; i < 4; i++)
-//   {
-//     printf("#\n");
-//   }
-  
-// }
-
-// {
-  // const int n = 5;
-//   int n;
-//   do
-//   {
-//     n = get_int("Size: "); 
-//   }
-//   while (n < 1);
-
-//   for(int i = 0; i < n; i++)
-//   {
-//     for(int j = 0; j < n; j++)
-//     {
-//       printf("#");
-//     }
-//     printf("\n");
-//   }
-// }
-
-// APPRAOCH PROBLEM WITH COMMENTS
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+#include <string.h>
 
-// Put at the top of the file the functions
+
 int get_size(void);
 void print_grid(int size);
-
 int main(void)
 {
-  // Get size of grid
-  int n = get_size();
+    // Get the height
+    int height = get_size();
 
-  // Print grid of bricks
-  print_grid(n);
+
+    // Print the bricks
+    print_grid(height);
+
 }
 
 int get_size(void)
 {
-  int n;
-  do
-  {
-    n = get_int("Size: ");
-  } while (n < 1);
-  return n;
-  
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8);
+    return height;
 }
 
 void print_grid(int size)
 {
-  for (int i = 0; i < size; i++)
-  {
-    for (int j = 0; j < size; j++)
+    int row, column, space;
+    for (row = 0; row < size; row++)
     {
-      printf("#");
+        for (space = 0; space < size - row - 1; space++)
+        {
+            printf(" ");
+        }
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 }
